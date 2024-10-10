f8e4754823d414eb8239e4b9dae95876396f1eef
CutLisa <cutneeslisa@gmail.com>
2024-10-10 16:28:13 +0600

    Update dalle-goat.js

--- scripts/cmds/dalle-goat.js
+++ scripts/cmds/dalle-goat.js
@@ -5,10 +5,10 @@ const baseApiUrl = async () => {
 }; 
 module.exports = {
   config: {
-    name: "dalle",
+    name: "dallepro",
     aliases: ["bing", "create", "imagine"],
     version: "1.0",
-    author: "Dipto",
+    author: "Lisax",
     countDown: 15,
     role: 0,
     description: "Generate images by Unofficial Dalle3",
@@ -22,13 +22,13 @@ module.exports = {
        //const cookies = "cookies here (_U value)";
 const cookies = ["1WMSMa5rJ9Jikxsu_KvCxWmb0m4AwilqsJhlkC1whxRDp2StLDR-oJBnLWpoppENES3sBh9_OeFE6BT-Kzzk_46_g_z_NPr7Du63M92maZmXZYR91ymjlxE6askzY9hMCdtX-9LK09sUsoqokbOwi3ldOlm0blR_0VLM3OjdHWcczWjvJ78LSUT7MWrdfdplScZbtHfNyOFlDIGkOKHI7Bg"];
 const randomCookie = cookies[Math.floor(Math.random() * cookies.length)];
-      const wait = api.sendMessage("Wait koro baby 😽", event.threadID);
+      const wait = api.sendMessage("𝙿𝚕𝚜 𝚠8 𝙸𝚖𝚐 𝙲𝚛𝚎𝚊𝚝 𝙻𝚒𝚜𝚊 😽", event.threadID);
       const response = await axios.get(`${await baseApiUrl()}/dalle?prompt=${prompt}&key=dipto008&cookies=${randomCookie}`);
 const imageUrls = response.data.imgUrls || [];
       if (!imageUrls.length) return api.sendMessage("Empty response or no images generated.", event.threadID, event.messageID);
       const images = await Promise.all(imageUrls.map(url => axios.get(url, { responseType: 'stream' }).then(res => res.data)));
     api.unsendMessage(wait.messageID);
-   api.sendMessage({ body: `✅ | Here's Your Generated Photo 😘`, attachment: images }, event.threadID, event.messageID);
+   api.sendMessage({ body: `✅ | 𝙷𝚎𝚛𝚎'𝚜 𝚈𝚘𝚞𝚛 𝙶𝚎𝚗𝚎𝚛𝚊𝚝𝚎𝚍 𝙿𝚑𝚘𝚝𝚘 😘`, attachment: images }, event.threadID, event.messageID);
     } catch (error) {
       console.error(error);
       api.sendMessage(`Generation failed!\nError: ${error.message}`, event.threadID, event.messageID);
